45cf1e95213835eddf143bcfc6bbe5812c9a08d4
Guilherme <gui_parracho@hotmail.com>
2025-08-11 20:38:10 -0300

    Criação de Getter and Setter do filme

--- Fillme.java
+++ Fillme.java
@@ -12,4 +12,36 @@ public class Filme {
         this.ano = ano;
         this.atores = new ArrayList<>();
     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public Diretor getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(Diretor diretor) {
+        this.diretor = diretor;
+    }
+
+    public List<Ator> getAtores() {
+        return atores;
+    }
+
+    public String toString() {
+        return titulo + " (" + ano + ") - Diretor: " + (diretor != null ? diretor.getNome() : "Nenhum") + ", Atores: " + atores ;
+    }
 }
